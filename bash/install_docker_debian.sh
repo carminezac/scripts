828738d1d22732485c63c05a0ac7407ff58fc7c3
Carmine Zacchia <carmine@PC-D-CZACCHIA.sagresgestioni.it>
2023-02-17 16:15:58 +0100

    fix rimosso obbligo di sudo per root

--- bash/install_docker_debian.sh
+++ bash/install_docker_debian.sh
@@ -9,7 +9,7 @@ apt-get install -y \
 curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update -y
 apt-get install -y docker-ce docker-ce-cli containerd.io
 docker volume create portainer_data
